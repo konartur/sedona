39f81e50a24857e40a5812dbddb754199a73ae36
suntikflood <suntikflood@gmail.com>
2020-07-06 21:16:40 +0300

    create gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,37 @@
-function defaultTask(cb) {
-  // place code for your default task here
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var sourcemaps = require("gulp-sourcemaps");
+var rename = require("gulp-rename");
+const browserSync = require("browser-sync").create();
+const autoprefixer = require("autoprefixer");
+var cssnano = require("cssnano");
+var postcss = require("gulp-postcss");
+
+sass.compiler = require("node-sass");
+
+function css() {
+  return gulp
+    .src("scss/**/*.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("css"))
+    .pipe(browserSync.stream());
+}
+
+function watch(cb) {
+  browserSync.init({
+    server: {
+      baseDir: ".",
+      index: "/index.html",
+    },
+  });
+  gulp.watch("scss/**/*.scss", css);
+  gulp.watch("index.html").on("change", browserSync.reload);
   cb();
 }
 
-exports.default = defaultTask;
+exports.css = css;
+exports.watch = watch;
