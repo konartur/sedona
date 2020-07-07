1baddf9784ec0dd6515682178e5c1da40984828a
suntikflood <suntikflood@gmail.com>
2020-07-07 21:15:52 +0300

    create basics gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,32 +6,63 @@ const browserSync = require("browser-sync").create();
 const autoprefixer = require("autoprefixer");
 var cssnano = require("cssnano");
 var postcss = require("gulp-postcss");
+var htmlmin = require("gulp-htmlmin");
+var del = require("del");
+const imagemin = require("gulp-imagemin");
+
+var dist = "public";
 
 sass.compiler = require("node-sass");
 
-function css() {
+gulp.task("styles", function () {
   return gulp
-    .src("scss/**/*.scss")
+    .src("src/styles/index.scss")
     .pipe(sourcemaps.init())
     .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
     .pipe(postcss([autoprefixer(), cssnano()]))
     .pipe(rename({ suffix: ".min" }))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest("css"))
+    .pipe(gulp.dest(dist))
     .pipe(browserSync.stream());
-}
+});
 
-function watch(cb) {
+gulp.task("watch", function () {
   browserSync.init({
     server: {
-      baseDir: ".",
-      index: "/index.html",
+      baseDir: "public",
     },
   });
-  gulp.watch("scss/**/*.scss", css);
-  gulp.watch("index.html").on("change", browserSync.reload);
-  cb();
-}
+  gulp.watch("src/styles/**/*.scss", gulp.series("styles"));
+  gulp
+    .watch("src/index.html", gulp.series("html"))
+    .on("change", browserSync.reload);
+});
 
-exports.css = css;
-exports.watch = watch;
+gulp.task("html", function () {
+  return gulp
+    .src("src/index.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest(dist));
+});
+
+gulp.task("clean", function () {
+  return del(dist);
+});
+
+gulp.task("img", function () {
+  return gulp
+    .src("src/images/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest(dist + "/images"));
+});
+
+gulp.task("font", function () {
+  return gulp.src("src/fonts").pipe(gulp.dest(dist));
+});
+
+gulp.task(
+  "build",
+  gulp.series("clean", gulp.parallel("html", "styles", "img", "font"))
+);
+
+gulp.task("default", gulp.series("build", "watch"));
